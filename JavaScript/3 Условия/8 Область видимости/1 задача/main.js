ce04ee64be3282ee339deaa746fbf684bb53346c
Anton <61190147+Anton2102@users.noreply.github.com>
2020-04-21 22:08:26 +0300

    21.04.2020(2)

---
+++ JavaScript/3 Условия/8 Область видимости/1 задача/main.js
@@ -0,0 +1,25 @@
+// Автор приведенного ниже кода хотел выполнить проверку возраста на достижение 18 лет.
+//
+// Код, однако, не работает. Исправьте ошибку автора кода.
+//
+// Вот проблемный код:
+//
+// let age = 17;
+//
+// if (age >= 18) {
+// 	let adult = true;
+// } else {
+// 	let adult = false;
+// }
+//
+// console.log(adult);
+
+let age = 17, adult;
+
+if (age >= 18) {
+  adult = true;
+} else {
+  adult = false;
+}
+
+console.log(adult);
---
+++ JavaScript/3 Условия/8 Область видимости/2 задача/main.js
@@ -0,0 +1,27 @@
+// Автор приведенного ниже кода хотел выполнить проверку возраста на достижение 18 лет.
+//
+// Код, однако, при любом значение возраста выводит в консоль значение undefined. Исправьте ошибку автора кода.
+//
+// Вот проблемный код:
+//
+// let age = 17;
+// let adult;
+//
+// if (age >= 18) {
+// 	let adult = true;
+// } else {
+// 	let adult = false;
+// }
+//
+// console.log(adult);
+
+let age = 17;
+let adult;
+
+if (age >= 18) {
+	adult = true;
+} else {
+	adult = false;
+}
+
+console.log(adult);
