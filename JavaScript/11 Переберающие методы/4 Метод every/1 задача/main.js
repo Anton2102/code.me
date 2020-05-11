d29c77922afd0c5fb4b80e1fee3503d3c209279b
Anton <61190147+Anton2102@users.noreply.github.com>
2020-05-11 20:37:35 +0300

    11.05.2020

---
+++ JavaScript/11 Переберающие методы/4 Метод every/1 задача/main.js
@@ -0,0 +1,4 @@
+// Дан массив с числами. Проверьте то, что все элементы в массиве больше нуля.
+let arr = [1,2,3];
+let result = arr.every(elem => elem > 0);
+console.log(result);
---
+++ JavaScript/11 Переберающие методы/5 Метод some/1 задача/main.js
@@ -0,0 +1,4 @@
+// Дан массив с числами. Проверьте то, что все элементы в массиве больше нуля.
+let arr = [1,2,3];
+let result = arr.some(elem => elem < 0);
+console.log(result);
