e2f95f751e0cb4e757e52c36c6110fc54dc438ad
Anton2102 <leshonkov1@gmail.com>
2020-07-06 20:58:43 +0300

    06.07.2020

---
+++ JavaScript - Высший учебник/регулярки/18 Метод test/2 задача/main.js
@@ -0,0 +1,2 @@
+// Определите, начинается ли переданная строка с http:// или с https://.
+console.log(/^http:\/\/|https:\/\//.test('https://orion-76.ru'));
---
+++ JavaScript - Высший учебник/регулярки/18 Метод test/3 задача/main.js
@@ -0,0 +1,2 @@
+// Определите, что ли заканчивается переданная строка расширением txt, html или php.
+console.log(/txt|html|php$/.test('main.html'));
